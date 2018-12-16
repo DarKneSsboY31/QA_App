8e794aae59f46813dd9cefb0012d972c6df86ddc
DarKneSsboY31 <blackimpact0123@outlook.jp>
2018-12-16 16:51:55 +0900

    QA_App　初めてのコミット

---
+++ app/src/main/java/jp/techacademy/suzuki/kenta/qa_app/Const.java
@@ -0,0 +1,11 @@
+package jp.techacademy.suzuki.kenta.qa_app;
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+
+    public static final String FavoriteKEY = "favorite"; //お気に入りかどうか確認するキー
+}
